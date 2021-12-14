4f6450c942348df598372c540d0d32188c71d848
unknown <dimasrosadi9029@gmail.com>
2021-12-14 08:01:22 +0700

    transform

--- src/data/index.js
+++ src/data/index.js
@@ -8,7 +8,7 @@ export const products = [
   },
   {
     id: 2,
-    img: Tabel,
+    img: "https://i.pinimg.com/originals/22/d0/f0/22d0f0ff02683598d617ea1e9850d372.png",
     link: "https://dimas-table.herokuapp.com/",
   },
   {
@@ -18,13 +18,13 @@ export const products = [
   },
   {
     id: 4,
-    img: MobileApp,
+    img: "https://cdn.dribbble.com/users/1998175/screenshots/16503601/media/cd2b899e0857b5cb16561ab2d712b3cd.jpg?compress=1&resize=1200x900",
     link: "https://www.figma.com/proto/UnjUcyVaSQXVcR2aKtVj2V/WarTop?node-id=1%3A32&scaling=scale-down&page-id=0%3A1",
   },
   {
     id: 5,
-    img: "",
-    link: "http://lama.dev",
+    img: " https://images.pexels.com/photos/4483775/pexels-photo-4483775.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    link: "https://inventory.chairulumam.com/login",
   },
   {
     id: 6,
